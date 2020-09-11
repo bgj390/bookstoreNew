efb72ca71419e99642198393310e7455ffdea259
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-09-11 15:53:16 +0300

    add redirect method

--- src/main/java/com/example/bookstoreNew/web/BookController.java
+++ src/main/java/com/example/bookstoreNew/web/BookController.java
@@ -28,7 +28,12 @@ public class BookController {
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
         return "addbook";
-    } 
+    }
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
