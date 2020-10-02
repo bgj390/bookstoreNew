54d2c80cdf30b31fead931b2d812c4d9cd965e25
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-10-02 17:34:47 +0300

    create users

--- src/main/java/com/example/bookstoreNew/BookstoreNewApplication.java
+++ src/main/java/com/example/bookstoreNew/BookstoreNewApplication.java
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 import com.example.bookstoreNew.model.Book;
 import com.example.bookstoreNew.model.BookRepository;
 import com.example.bookstoreNew.model.Category;
+import com.example.bookstoreNew.model.User;
 import com.example.bookstoreNew.model.CategoryRepository;
+import com.example.bookstoreNew.model.UserRepository;
 
 @SpringBootApplication
 public class BookstoreNewApplication {
@@ -22,19 +24,27 @@ public class BookstoreNewApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a few books");
-		
+			
+			// Create categories
 			crepository.save(new Category("Autobiography"));
 			crepository.save(new Category("Science Fiction"));
 			crepository.save(new Category("Horror"));
 			crepository.save(new Category("Satire"));
 			
+			// Create books
 			brepository.save(new Book("Happoa lapsille", "Flea", 2019, "978-952-01-1967-6", 20.90, crepository.findByName("Autobiography").get(0)));
 			brepository.save(new Book("Tyttö bändissä", "Kim Gordon", 2015, "978-952-01-1015-4", 9.95, crepository.findByName("Autobiography").get(0)));
 			brepository.save(new Book("science fiction-valikoima", "Isaac Asimov", 1981, "996611-1-01", 12.50, crepository.findByName("Science Fiction").get(0)));
 				
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			
 				log.info("fetch all books");
 				for (Book book : brepository.findAll()) {
 					log.info(book.toString());
