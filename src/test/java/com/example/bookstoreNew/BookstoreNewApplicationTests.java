64076f43c9708f6ffe85e76bf739b57f68e9dab2
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-10-15 14:50:44 +0300

    Add test

--- src/test/java/com/example/bookstoreNew/BookstoreNewApplicationTests.java
+++ src/test/java/com/example/bookstoreNew/BookstoreNewApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.bookstoreNew;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.bookstoreNew.web.BookController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+/**
+ * Testing that the context is creating your controller
+ * 
+ * @author h01270
+ *
+ */
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class BookstoreNewApplicationTests {
+public class BookstoreNewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private BookController controller;
 
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }	
 }
