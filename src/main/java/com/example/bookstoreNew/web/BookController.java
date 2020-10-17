fc87a049f89ec1486a794ced4aa867dfb561c4f0
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-10-17 11:39:17 +0300

    switch to hasAuthority

--- src/main/java/com/example/bookstoreNew/web/BookController.java
+++ src/main/java/com/example/bookstoreNew/web/BookController.java
@@ -57,7 +57,7 @@ public class BookController {
         return "redirect:booklist";
     }
     @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
